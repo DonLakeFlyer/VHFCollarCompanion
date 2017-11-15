992fc76e171087570d9f6c83542cf234173e18aa
DonLakeFlyer <don@thegagnes.com>
2017-11-15 14:05:31 -0800

    settings

--- Tools.py
+++ Tools.py
@@ -1,11 +1,11 @@
 from multiprocessing import Queue
 
 class Tools:
-    def __init__(self):
-        self.mavlinkThread = None
-        self.vehicle = None
-        self.directionFinder = None
-        self.pulseSender= None
+	def __init__(self):
+		self.mavlinkThread = None
+		self.vehicle = None
+		self.directionFinder = None
+		self.pulseSender= None
 		self.pulseQueue = Queue()
 		self.leftFregQueue = Queue()
 		self.rightFreqQueue = Queue()
