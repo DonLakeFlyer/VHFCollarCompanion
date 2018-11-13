b650aee428db72089ac79602834eb8d55b98a191
Don Gagne <dongagne@outlook.com>
2018-11-13 13:32:53 -0800

    Switch to C++ PulseDetect

--- PulseDetectCmdLine.py
+++ PulseDetectCmdLine.py
@@ -3,14 +3,13 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Pulsedetectcmdline
-# Generated: Mon Nov 12 17:07:34 2018
+# Generated: Tue Nov 13 12:44:53 2018
 ##################################################
 
 import os
 import sys
 sys.path.append(os.environ.get('GRC_HIER_PATH', os.path.expanduser('~/.grc_gnuradio')))
 
-from GRCEmbeddedPulseDetect import blk
 from PulseDetectBase import PulseDetectBase  # grc-generated hier_block
 from gnuradio import blocks
 from gnuradio import eng_notation
@@ -18,6 +17,7 @@ from gnuradio import gr
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
+import VHFPulseDetect
 import cmath
 import math
 
@@ -44,6 +44,7 @@ class PulseDetectCmdLine(gr.top_block):
         ##################################################
         self.blocks_vector_sink_x_1 = blocks.vector_sink_f(1)
         self.blocks_vector_sink_x_0 = blocks.vector_sink_c(1)
+        self.VHFPulseDetect_pulse_detect__ff_0 = VHFPulseDetect.pulse_detect__ff()
         self.PulseDetectBase = PulseDetectBase(
             freq_shift=0,
             lna_gain=11,
@@ -52,14 +53,13 @@ class PulseDetectCmdLine(gr.top_block):
             vga_gain=15,
             wnT=math.pi/4.0*0+0.635,
         )
-        self.GRCEmbeddedPulseDetect = blk(sample_rate=final_samp_rate)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.GRCEmbeddedPulseDetect, 0), (self.blocks_vector_sink_x_1, 0))    
-        self.connect((self.PulseDetectBase, 0), (self.GRCEmbeddedPulseDetect, 0))    
+        self.connect((self.PulseDetectBase, 0), (self.VHFPulseDetect_pulse_detect__ff_0, 0))    
         self.connect((self.PulseDetectBase, 1), (self.blocks_vector_sink_x_0, 0))    
+        self.connect((self.VHFPulseDetect_pulse_detect__ff_0, 0), (self.blocks_vector_sink_x_1, 0))    
 
     def get_pulse_freq(self):
         return self.pulse_freq
