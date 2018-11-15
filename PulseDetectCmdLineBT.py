138b56ae8c5d88a710553bfbc7d27e6dab8a58dd
Don Gagne <dongagne@outlook.com>
2018-11-14 19:13:32 -0800

    channel_index support

--- PulseDetectCmdLineBT.py
+++ PulseDetectCmdLineBT.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Pulsedetectcmdlinebt
-# Generated: Wed Nov 14 13:44:12 2018
+# Generated: Wed Nov 14 19:09:22 2018
 ##################################################
 
 import os
@@ -25,12 +25,13 @@ import math
 
 class PulseDetectCmdLineBT(gr.top_block):
 
-    def __init__(self, pulse_freq=146e6, samp_rate=3e6):
+    def __init__(self, channel_index=0, pulse_freq=146e6, samp_rate=3e6):
         gr.top_block.__init__(self, "Pulsedetectcmdlinebt")
 
         ##################################################
         # Parameters
         ##################################################
+        self.channel_index = channel_index
         self.pulse_freq = pulse_freq
         self.samp_rate = samp_rate
 
@@ -44,7 +45,7 @@ class PulseDetectCmdLineBT(gr.top_block):
         # Blocks
         ##################################################
         self.blocks_vector_sink_x_0 = blocks.vector_sink_c(1)
-        self.VHFPulseSender_bt_sender_f_0 = VHFPulseSender.bt_sender_f()
+        self.VHFPulseSender_bt_sender_f_0 = VHFPulseSender.bt_sender_f(channel_index)
         self.VHFPulseDetect_pulse_detect__ff_0 = VHFPulseDetect.pulse_detect__ff()
         self.PulseDetectBase = PulseDetectBase(
             freq_shift=0,
@@ -61,6 +62,12 @@ class PulseDetectCmdLineBT(gr.top_block):
         self.connect((self.PulseDetectBase, 0), (self.VHFPulseDetect_pulse_detect__ff_0, 0))    
         self.connect((self.PulseDetectBase, 1), (self.blocks_vector_sink_x_0, 0))    
         self.connect((self.VHFPulseDetect_pulse_detect__ff_0, 0), (self.VHFPulseSender_bt_sender_f_0, 0))    
+
+    def get_channel_index(self):
+        return self.channel_index
+
+    def set_channel_index(self, channel_index):
+        self.channel_index = channel_index
 
     def get_pulse_freq(self):
         return self.pulse_freq
@@ -91,7 +98,10 @@ class PulseDetectCmdLineBT(gr.top_block):
 
 
 def argument_parser():
-    parser = OptionParser(usage="%prog: [options]", option_class=eng_option)
+    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    parser.add_option(
+        "", "--channel-index", dest="channel_index", type="intx", default=0,
+        help="Set Channel Index [default=%default]")
     parser.add_option(
         "", "--pulse-freq", dest="pulse_freq", type="eng_float", default=eng_notation.num_to_str(146e6),
         help="Set pulse_freq [default=%default]")
@@ -107,7 +117,7 @@ def main(top_block_cls=PulseDetectCmdLineBT, options=None):
     if gr.enable_realtime_scheduling() != gr.RT_OK:
         print "Error: failed to enable real-time scheduling."
 
-    tb = top_block_cls(pulse_freq=options.pulse_freq, samp_rate=options.samp_rate)
+    tb = top_block_cls(channel_index=options.channel_index, pulse_freq=options.pulse_freq, samp_rate=options.samp_rate)
     tb.start()
     try:
         raw_input('Press Enter to quit: ')
