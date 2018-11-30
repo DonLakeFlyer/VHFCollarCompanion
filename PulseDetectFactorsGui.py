d350ba7f13f1c582057564141918748838e18960
Don Gagne <dongagne@outlook.com>
2018-11-30 13:36:07 -0800

    New tool

---
+++ PulseDetectFactorsGui.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Pulsedetectfactorsgui
+# Generated: Fri Nov 30 13:32:43 2018
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+import os
+import sys
+sys.path.append(os.environ.get('GRC_HIER_PATH', os.path.expanduser('~/.grc_gnuradio')))
+
+from PulseDetectBase import PulseDetectBase  # grc-generated hier_block
+from PyQt4 import Qt
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import qtgui
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from optparse import OptionParser
+import VHFPulseDetect
+import VHFPulseSender
+import cmath
+import math
+import sip
+
+
+class PulseDetectFactorsGui(gr.top_block, Qt.QWidget):
+
+    def __init__(self, pulse_freq=146e6, samp_rate=3e6):
+        gr.top_block.__init__(self, "Pulsedetectfactorsgui")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Pulsedetectfactorsgui")
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "PulseDetectFactorsGui")
+        self.restoreGeometry(self.settings.value("geometry").toByteArray())
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.pulse_freq = pulse_freq
+        self.samp_rate = samp_rate
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.total_decimation = total_decimation = 4*8*8
+        self.final_samp_rate = final_samp_rate = samp_rate/total_decimation
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+        	int(final_samp_rate*10.0), #size
+        	final_samp_rate, #samp_rate
+        	"Pulse Processing Views", #name
+        	6 #number of inputs
+        )
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+        
+        self.qtgui_time_sink_x_0.set_y_label("Amplitude", "")
+        
+        self.qtgui_time_sink_x_0.enable_tags(-1, True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(True)
+        self.qtgui_time_sink_x_0.enable_grid(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        
+        if not True:
+          self.qtgui_time_sink_x_0.disable_legend()
+        
+        labels = ["Pulse Detect", "Pulse Raw", "Moving Avg", "Moving Var", "Moving StdDev",
+                  "Threshold", "Re{Ref Out}", "Im{Ref Out}", "Ref Frequency", "zero"]
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "magenta",
+                  "cyan", "dark green", "dark red", "Dark Blue", "Dark Blue"]
+        styles = [3, 1, 1, 1, 1,
+                  1, 1, 1, 3, 0]
+        markers = [-1, -1, -1, -1, -1,
+                   -1, -1, -1, -1, -1]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        
+        for i in xrange(6):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+        
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win, 9,0,1,1)
+        self.blocks_vector_sink_x_0 = blocks.vector_sink_c(1)
+        self.VHFPulseSender_udp_sender_f_0 = VHFPulseSender.udp_sender_f()
+        self.VHFPulseDetect_pulse_detect__ff_0 = VHFPulseDetect.pulse_detect__ff()
+        self.PulseDetectBase = PulseDetectBase(
+            freq_shift=0,
+            lna_gain=11,
+            mixer_gain=11,
+            pulse_freq=pulse_freq,
+            vga_gain=1,
+            wnT=math.pi/4.0*0+0.635,
+        )
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.PulseDetectBase, 0), (self.VHFPulseDetect_pulse_detect__ff_0, 0))    
+        self.connect((self.PulseDetectBase, 1), (self.blocks_vector_sink_x_0, 0))    
+        self.connect((self.VHFPulseDetect_pulse_detect__ff_0, 0), (self.VHFPulseSender_udp_sender_f_0, 0))    
+        self.connect((self.VHFPulseDetect_pulse_detect__ff_0, 2), (self.qtgui_time_sink_x_0, 2))    
+        self.connect((self.VHFPulseDetect_pulse_detect__ff_0, 4), (self.qtgui_time_sink_x_0, 4))    
+        self.connect((self.VHFPulseDetect_pulse_detect__ff_0, 3), (self.qtgui_time_sink_x_0, 3))    
+        self.connect((self.VHFPulseDetect_pulse_detect__ff_0, 1), (self.qtgui_time_sink_x_0, 1))    
+        self.connect((self.VHFPulseDetect_pulse_detect__ff_0, 0), (self.qtgui_time_sink_x_0, 0))    
+        self.connect((self.VHFPulseDetect_pulse_detect__ff_0, 5), (self.qtgui_time_sink_x_0, 5))    
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "PulseDetectFactorsGui")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+
+    def get_pulse_freq(self):
+        return self.pulse_freq
+
+    def set_pulse_freq(self, pulse_freq):
+        self.pulse_freq = pulse_freq
+        self.PulseDetectBase.set_pulse_freq(self.pulse_freq)
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.set_final_samp_rate(self.samp_rate/self.total_decimation)
+
+    def get_total_decimation(self):
+        return self.total_decimation
+
+    def set_total_decimation(self, total_decimation):
+        self.total_decimation = total_decimation
+        self.set_final_samp_rate(self.samp_rate/self.total_decimation)
+
+    def get_final_samp_rate(self):
+        return self.final_samp_rate
+
+    def set_final_samp_rate(self, final_samp_rate):
+        self.final_samp_rate = final_samp_rate
+        self.qtgui_time_sink_x_0.set_samp_rate(self.final_samp_rate)
+
+
+def argument_parser():
+    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    parser.add_option(
+        "", "--pulse-freq", dest="pulse_freq", type="eng_float", default=eng_notation.num_to_str(146e6),
+        help="Set pulse_freq [default=%default]")
+    parser.add_option(
+        "", "--samp-rate", dest="samp_rate", type="eng_float", default=eng_notation.num_to_str(3e6),
+        help="Set samp_rate [default=%default]")
+    return parser
+
+
+def main(top_block_cls=PulseDetectFactorsGui, options=None):
+    if options is None:
+        options, _ = argument_parser().parse_args()
+    if gr.enable_realtime_scheduling() != gr.RT_OK:
+        print "Error: failed to enable real-time scheduling."
+
+    from distutils.version import StrictVersion
+    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls(pulse_freq=options.pulse_freq, samp_rate=options.samp_rate)
+    tb.start()
+    tb.show()
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
+    qapp.exec_()
+
+
+if __name__ == '__main__':
+    main()
