e3c1da3b59415541d4b806d3d771a28a58f0bb01
Don Gagne <dongagne@outlook.com>
2018-12-25 14:10:07 -0800

    Update

--- PulseDetectCmdLineBT.py
+++ PulseDetectCmdLineBT.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Pulsedetectcmdlinebt
-# Generated: Fri Nov 16 12:09:11 2018
+# Generated: Tue Dec 25 14:08:19 2018
 ##################################################
 
 import os
@@ -21,40 +21,43 @@ import VHFPulseDetect
 import VHFPulseSender
 import cmath
 import math
-import signal
 
 
 class PulseDetectCmdLineBT(gr.top_block):
 
-    def __init__(self, pulse_freq=146e6, samp_rate=3e6, channel_index=0, vga_gain=15):
+    def __init__(self, channel_index=0, pulse_freq=146e6, samp_rate=3e6, vga_gain=15):
         gr.top_block.__init__(self, "Pulsedetectcmdlinebt")
 
         ##################################################
         # Parameters
         ##################################################
+        self.channel_index = channel_index
         self.pulse_freq = pulse_freq
         self.samp_rate = samp_rate
-        self.channel_index = channel_index
         self.vga_gain = vga_gain
 
         ##################################################
         # Variables
         ##################################################
-        self.total_decimation = total_decimation = 4*8*8
+        self.total_decimation = total_decimation = 16*16*4
         self.final_samp_rate = final_samp_rate = samp_rate/total_decimation
 
         ##################################################
         # Blocks
         ##################################################
+        self.blocks_vector_sink_x_1_3 = blocks.vector_sink_f(1)
+        self.blocks_vector_sink_x_1_2 = blocks.vector_sink_f(1)
+        self.blocks_vector_sink_x_1_1 = blocks.vector_sink_f(1)
+        self.blocks_vector_sink_x_1_0 = blocks.vector_sink_f(1)
+        self.blocks_vector_sink_x_1 = blocks.vector_sink_f(1)
         self.blocks_vector_sink_x_0 = blocks.vector_sink_c(1)
         self.VHFPulseSender_bt_sender_f_0 = VHFPulseSender.bt_sender_f(channel_index)
-        self.VHFPulseDetect_pulse_detect__ff_0 = VHFPulseDetect.pulse_detect__ff()
+        self.VHFPulseDetect_pulse_detect__ff_0 = VHFPulseDetect.pulse_detect__ff(2.5, 35)
         self.PulseDetectBase = PulseDetectBase(
             freq_shift=0,
-            lna_gain=11,
-            mixer_gain=11,
+            gain=21,
             pulse_freq=pulse_freq,
-            vga_gain=vga_gain,
+            samp_rate=3e6,
             wnT=math.pi/4.0*0+0.635,
         )
 
@@ -63,11 +66,18 @@ class PulseDetectCmdLineBT(gr.top_block):
         ##################################################
         self.connect((self.PulseDetectBase, 0), (self.VHFPulseDetect_pulse_detect__ff_0, 0))    
         self.connect((self.PulseDetectBase, 1), (self.blocks_vector_sink_x_0, 0))    
-        self.connect((self.VHFPulseDetect_pulse_detect__ff_0, 0), (self.VHFPulseSender_bt_sender_f_0, 0))
+        self.connect((self.VHFPulseDetect_pulse_detect__ff_0, 0), (self.VHFPulseSender_bt_sender_f_0, 0))    
+        self.connect((self.VHFPulseDetect_pulse_detect__ff_0, 1), (self.blocks_vector_sink_x_1, 0))    
+        self.connect((self.VHFPulseDetect_pulse_detect__ff_0, 2), (self.blocks_vector_sink_x_1_0, 0))    
+        self.connect((self.VHFPulseDetect_pulse_detect__ff_0, 3), (self.blocks_vector_sink_x_1_1, 0))    
+        self.connect((self.VHFPulseDetect_pulse_detect__ff_0, 4), (self.blocks_vector_sink_x_1_2, 0))    
+        self.connect((self.VHFPulseDetect_pulse_detect__ff_0, 5), (self.blocks_vector_sink_x_1_3, 0))    
 
-        # The following line is modified from the .grc output. It connects the two objects
-        # such that bt_sender can change parameters in PulseDetectBase.
-        self.VHFPulseSender_bt_sender_f_0.setPulseDetectBase(self.PulseDetectBase)
+    def get_channel_index(self):
+        return self.channel_index
+
+    def set_channel_index(self, channel_index):
+        self.channel_index = channel_index
 
     def get_pulse_freq(self):
         return self.pulse_freq
@@ -83,18 +93,11 @@ class PulseDetectCmdLineBT(gr.top_block):
         self.samp_rate = samp_rate
         self.set_final_samp_rate(self.samp_rate/self.total_decimation)
 
-    def get_channel_index(self):
-        return self.channel_index
-
-    def set_channel_index(self, channel_index):
-        self.channel_index = channel_index
-
     def get_vga_gain(self):
         return self.vga_gain
 
     def set_vga_gain(self, vga_gain):
         self.vga_gain = vga_gain
-        self.PulseDetectBase.set_vga_gain(self.vga_gain)
 
     def get_total_decimation(self):
         return self.total_decimation
@@ -113,14 +116,14 @@ class PulseDetectCmdLineBT(gr.top_block):
 def argument_parser():
     parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
     parser.add_option(
+        "", "--channel-index", dest="channel_index", type="intx", default=0,
+        help="Set Channel Index [default=%default]")
+    parser.add_option(
         "", "--pulse-freq", dest="pulse_freq", type="eng_float", default=eng_notation.num_to_str(146e6),
         help="Set pulse_freq [default=%default]")
     parser.add_option(
         "", "--samp-rate", dest="samp_rate", type="eng_float", default=eng_notation.num_to_str(3e6),
         help="Set samp_rate [default=%default]")
-    parser.add_option(
-        "", "--channel-index", dest="channel_index", type="intx", default=0,
-        help="Set Channel Index [default=%default]")
     parser.add_option(
         "", "--vga-gain", dest="vga_gain", type="intx", default=15,
         help="Set VGA Gain [default=%default]")
@@ -133,10 +136,12 @@ def main(top_block_cls=PulseDetectCmdLineBT, options=None):
     if gr.enable_realtime_scheduling() != gr.RT_OK:
         print "Error: failed to enable real-time scheduling."
 
-    tb = top_block_cls(pulse_freq=options.pulse_freq, samp_rate=options.samp_rate, channel_index=options.channel_index, vga_gain=options.vga_gain)
+    tb = top_block_cls(channel_index=options.channel_index, pulse_freq=options.pulse_freq, samp_rate=options.samp_rate, vga_gain=options.vga_gain)
     tb.start()
-    while True:
-        signal.pause()
+    try:
+        raw_input('Press Enter to quit: ')
+    except EOFError:
+        pass
     tb.stop()
     tb.wait()
 
