8e42b4d197c810044d8dcc43dbde6645fb56812e
DonLakeFlyer <dongagne@outlook.com>
2019-09-11 10:45:33 -0700

    Revert "Add startup delay"
    This reverts commit b9f7ab36619693295742bf0d3fae41c72f57647d.

--- MavlinkThread.py
+++ MavlinkThread.py
@@ -2,7 +2,6 @@ import threading
 import math
 import logging
 import subprocess
-import time
 
 import Vehicle
 
@@ -56,9 +55,6 @@ class MavlinkThread (threading.Thread):
 		self.pulseProcess = None
 
 	def run(self):
-		# Delay to prevent mavlink connection from getting hosed at startup
-		logging.debug("Startup delay")
-		time.sleep(20)
 		# Start a mavlink connection to get the system id from the heartbeat
 		haveHeartbeat = False
 		while not haveHeartbeat:
